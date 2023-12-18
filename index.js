b3596914c3b3bb2403eecd689ccf54b9f8044bce
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2023-12-18 17:33:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("no init params")
 
+window.codioIDE.menu.addItem(
+  {id: 'education'},
+  {id: 'customId', title: 'Custom button!!', callback: () => console.log('custom btn clicked')}
+)
