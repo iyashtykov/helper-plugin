2e3691b704d4a0bbf056b395cc08d0606d1fcdb5
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2023-12-18 17:42:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 console.log("all")
 
-window.codioIDE.menu.addItem(
-  {id: 'customId', title: 'Custom button!!', callback: () => console.log('custom btn clicked')}
-)
+const MENU_ID = 'custom_menu_id'
+
+const addMenuItem = (parentDescriptor, item) => {
+  window.codioIDE.menu.addItem(parentDescriptor, item)
+}
+
+const updateMenuItem = (itemDescriptor, item) => {
+  window.codioIDE.menu.updateItem(itemDescriptor, item)
+}
+
+const removeMenuItem = (itemDescriptor) => {
+  window.codioIDE.menu.removeItem(itemDescriptor)
+}
+
+const style = `
+    width: 500px;
+    height: 200px;
+    z-index: 10000;
+    position: absolute;
+    left: 100px;
+    top: 100px;
+    background-color: white;
+    border: 2px solid red;
+    padding: 20px;
+`
+
+const initFunc = () => {
+  console.log('init!!!')
+  const modal = document.createElement('div')
+  modal.id = 'customModalId'
+  modal.style = style
+  const title = document.createElement('h2')
+  title.innerHTML = 'Modal title'
+  modal.append(title)
+  const buttonAdd = document.createElement('button')
+  buttonAdd.type = 'button'
+  buttonAdd.innerHTML = 'Add custom menu item to Tools'
+  const changeButtonBgCallback = () => {
+    buttonAdd.style = `background-color: #${Math.floor(Math.random()*16777215).toString(16)}`
+  }
+  buttonAdd.onclick = () => {
+    addMenuItem({title: 'Tools'}, {id: MENU_ID, title: 'Change button bg', callback: changeButtonBgCallback})
+  }
+  modal.append(buttonAdd)
+  const buttonRename = document.createElement('button')
+  buttonRename.type = 'button'
+  buttonRename.innerHTML = 'Rename custom menu item'
+  buttonRename.onclick = () => updateMenuItem({id: MENU_ID}, {title: 'new custom title'})
+  modal.append(buttonRename)
+  const buttonDelete = document.createElement('button')
+  buttonDelete.type = 'button'
+  buttonDelete.innerHTML = 'Delete custom menu item'
+  buttonDelete.onclick = () => removeMenuItem({id: MENU_ID})
+  modal.append(buttonDelete)
+  document.querySelector('body').append(modal)
+}
+
+window.addEventListener('load', () => {
+  setTimeout(initFunc, 3000)
+})
